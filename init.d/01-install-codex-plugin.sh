be4e54a59178e437e997337e9a85f2dcf70b19a3
Tasko Olevski <16360283+olevski@users.noreply.github.com>
2026-06-22 16:06:09 +0200

    chore: minor fix

--- init.d/01-install-codex-plugin.sh
+++ init.d/01-install-codex-plugin.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-codex plugin marketplace add https://github.com/openai/plugins
 if [ -n "$CODEX_HOME" ]; then
   mkdir -p "$CODEX_HOME"
 fi
+codex plugin marketplace add https://github.com/openai/plugins
 codex plugin add build-web-data-visualization@openai-curated
